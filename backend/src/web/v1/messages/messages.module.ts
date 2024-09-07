0226b3bc673420bfff5d5ffdca5f77cb756b8419
ActuallyPanda56 <nietoc0595@gmail.com>
2024-09-07 18:17:07 -0500

    message socket W/frontend connection

--- backend/src/web/v1/messages/messages.module.ts
+++ backend/src/web/v1/messages/messages.module.ts
@@ -6,10 +6,11 @@ import { MessagesService } from './messages.service';
 import { User } from 'src/entities/users.entity';
 import { Chat } from 'src/entities/chats.entity';
 import { ChatsService } from '../chats/chats.service';
+import { MessagesGateway } from './messages.gateway';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Message, User, Chat])],
   controllers: [MessagesController],
-  providers: [MessagesService, ChatsService],
+  providers: [MessagesService, ChatsService, MessagesGateway],
 })
 export class MessagesModule {}
