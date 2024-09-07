82e1afcbb4981a84ef5aed91f96829d827bd1551
ActuallyPanda56 <157256632+ActuallyPanda56@users.noreply.github.com>
2024-09-07 18:19:44 -0500

    Revert "Chat/message contollers"

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,26 +1,10 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { UsersModule } from './web/v1/users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { MessagesModule } from './web/v1/messages/messages.module';
-import 'dotenv/config';
-import { config } from './config';
-import { ChatsModule } from './web/v1/chats/chats.module';
-import { LoggingMiddleware } from './middlewares/logging.middleware';
-import { AuthModule } from './web/v1/auth/auth.module';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(config.database),
-    UsersModule,
-    MessagesModule,
-    ChatsModule,
-    AuthModule,
-  ],
-  controllers: [],
-  providers: [],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggingMiddleware).forRoutes('*');
-  }
-}
+export class AppModule {}
